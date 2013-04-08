498d8f0d5f7f5f37bbf02d61a3aab00296f336f3
normalocity <jefflunt@gmail.com>
2013-04-07 22:33:36 -0500

    Moved up the initialization of the logger
    - The logger was being used before it was initialized, and as a result it wasn't
      working

--- lib/how_slow.rb
+++ lib/how_slow.rb
@@ -1,4 +1,5 @@
+METRICS_LOGGER = Logger.new("#{Rails.root}/log/metrics.log")
+
 require File.join(File.dirname(__FILE__), 'how_slow', 'version')
 require File.join(File.dirname(__FILE__), 'how_slow', 'event_subscriptions')
 
-METRICS_LOGGER = Logger.new("#{Rails.root}/log/metrics.log")
