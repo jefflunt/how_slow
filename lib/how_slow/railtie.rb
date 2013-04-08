0cb90d31da6749754cb4f9f8242724c6250852c1
normalocity <jefflunt@gmail.com>
2013-04-07 23:32:15 -0500

    Moved notification subscription to Railtie
    - Moved the ActiveSupport::Notifications event subscriptions to the gem's
      Railtie, in order to get it to subcribe when the gem is loaded.
    - Got rid of the separate ActiveSupport::Notifications subscription code/file

--- lib/how_slow/railtie.rb
+++ lib/how_slow/railtie.rb
@@ -1,7 +1,41 @@
 module HowSlow
+  # Configuration defaults
+  @config = {
+              :log_file => "#{Rails.root}/log/metrics.log",
+              :event_subscriptions => [/process_action.action_controller/]
+            }
+  @valid_config_keys = @config.keys
+  @logger = nil
+
+  def self.configure(opts = {})
+    opts.each {|k,v| @config[k.to_sym] = v if @valid_config_keys.include? k.to_sym}
+  end
+
+  def self.config
+    @config
+  end
+
   class Railtie < Rails::Railtie
     initializer "railtie.configure_rails_initialization" do |app|
-      METRICS_LOGGER = Logger.new("#{app.root}/log/metrics.log")
+      @logger = Logger.new(@config[:log_file])
+
+      @config[:event_subscriptions].each do |event_name|
+        ActiveSupport::Notifications.subscribe event_name do |name, start, finish, id, payload|
+          event_metrics = {}
+          event_metrics['datetime'] = Time.now.to_s
+          event_metrics['event_name'] = 'all_requests'
+          event_metrics['status'] = payload[:status]
+          
+          total_runtime = event_metrics['total_runtime'] = (finish-start)*1000
+          db_runtime = event_metrics['db_runtime'] = payload[:db_runtime] || 0.0 
+          view_runtime = event_metrics['view_runtime'] = payload[:view_runtime] || 0.0 
+          
+          event_metrics['other_runtime'] = (total_runtime-db_runtime-view_runtime)
+                
+          event_metrics['params'] = payload[:params]
+          @config[:log_file].fo(event_metrics.to_json)
+        end
+      end
     end
   end
 end
