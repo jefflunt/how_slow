2ec1483b93489cd0ef04d1e486ceee95e9c685d3
normalocity <jefflunt@gmail.com>
2013-04-07 22:25:01 -0500

    Moved init of logger, and updated .gitignore
    - The initialization of the metrics logged was moved into the lib/how_slow.rb
      file so that I can ensure that the logged is initialized just once when the
      gem is loaded
    - Updated the .gitinore file to ignore local builds of the gem

--- lib/how_slow.rb
+++ lib/how_slow.rb
@@ -1,2 +1,4 @@
 require File.join(File.dirname(__FILE__), 'how_slow', 'version')
 require File.join(File.dirname(__FILE__), 'how_slow', 'event_subscriptions')
+
+METRICS_LOGGER = Logger.new("#{Rails.root}/log/metrics.log")
--- lib/how_slow/event_subscriptions.rb
+++ lib/how_slow/event_subscriptions.rb
@@ -1,6 +1,4 @@
 ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |name, start, finish, id, payload|
-  @@metrics_logger ||= Logger.new("#{Rails.root}/log/metrics.log")
-
   event_metrics = {}
   event_metrics['datetime'] = Time.now.to_s
   event_metrics['event_name'] = 'all_requests'
@@ -13,6 +11,6 @@ ActiveSupport::Notifications.subscribe /process_action.action_controller/ do |na
   event_metrics['other_runtime'] = (total_runtime-db_runtime-view_runtime)
         
   event_metrics['params'] = payload[:params]
-  @@metrics_logger.info(event_metrics.to_json)
+  METRICS_LOGGER.info(event_metrics.to_json)
 end
 
