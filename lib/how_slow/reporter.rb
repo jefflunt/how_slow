e4c9397e9750bf56aba319bf7ccd630f7b5dc65f
normalocity <jefflunt@gmail.com>
2013-04-10 20:47:18 -0500

    Added some debug lines

--- lib/how_slow/reporter.rb
+++ lib/how_slow/reporter.rb
@@ -31,7 +31,10 @@ module HowSlow
       @metrics['actions'] = []
       @metrics['counters'] = []
 
-      all_logged_metrics = File.read(HowSlow.full_path_to_log_file).lines.each{|line| JSON.parse(line) unless line.start_with?("#")}
+      all_logged_metrics = File.read(HowSlow.full_path_to_log_file).lines.collect{|line| JSON.parse(line) unless line.start_with?("#")}
+      puts all_logged_metrics.class
+      puts all_logged_metrics.first.inspect
+      puts all_logged_metrics.inspect
       all_logged_metrics.reject!{|metric| Time.parse(metric['datetime']) < reject_older_than} unless reject_older_than.nil?
       all_logged_metrics.each{|metric| @metrics[metric['type']].push(metric)}
     end
