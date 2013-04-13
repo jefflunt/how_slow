44a53d53b5c9624ebbe108ebc710556e8a6ca706
normalocity <jefflunt@gmail.com>
2013-04-13 16:13:43 -0500

    Added documentation to the reporter methods. Corrected some hash keys.

--- lib/how_slow/reporter.rb
+++ lib/how_slow/reporter.rb
@@ -1,12 +1,41 @@
 module HowSlow
   @metrics = {
-    'actions' => [],
-    'counters' => []
+    'action' => [],
+    'counter' => []
   }
- 
-  # Gives you a list of the slowest actions by total_runtime, most slow first.
+
+  # Returns a hash of the metrics that have been logged to the log file. The 
+  # metics must first be built by callng the `rebuld_metrics` method. The
+  # structure of the returned hash is:
   #
-  # So, if you have the following set of action metrics...
+  #   {
+  #     'action' => [
+  #       {'type' => 'action', ... },
+  #       {'type' => 'action', ... },
+  #       ...
+  #     ],
+  #     'counter' => [
+  #       {'type' => 'counter', ...},
+  #       ...
+  #     ]
+  #   }
+  #
+  # The order of the metrics under each metric type key ('action', or 'counter')
+  # is the same as the order in which they were written to the log file. NOTE
+  # that this doesn't mean they're in perfect chronological order by the
+  # datetime of the action since long-running actions, multiple threads,  or
+  # multiple app server instances, all writing to the same log file can result
+  # in metrics being logged somewhat out of strict chronological order. If you
+  # need the metrics to be in ANY kind of strict order you should enforce this
+  # yourself by sorting them by the attribute(s) that matters to you.
+  #
+  def self.metrics
+    @metrics
+  end
+ 
+  # Gives you a list of the slowest actions by total_runtime, slowest first.
+  #
+  # So, if you have the following set of action metrics in the log file...
   #
   #   {'type':'action', 'total_runtime':123.0, ... }
   #   {'type':'action', 'total_runtime':456.7, ... }
@@ -15,10 +44,10 @@ module HowSlow
   #   {'type':'action', 'total_runtime':3.0,   ... }
   # 
   # ...then
-  #   slowest_actions(nil, 2)
-  #   => [{'type':'action', 'total_runtime':456.7}, {'type':'action', 'total_runtime':123.0}]A
   #
-  # So, you get an array of 'action' typ
+  #   slowest_actions(nil, 2)
+  #   => [{'type':'action', 'total_runtime':456.7}, 
+  #       {'type':'action', 'total_runtime':123.0}]
   # 
   def self.slowest_actions(reject_older_than=7.days.ago, number_of_slowest=5)
     rebuild_metrics(reject_older_than)
@@ -26,16 +55,28 @@ module HowSlow
     sorted_metrics.last(number_of_slowest).reverse
   end
 
-  private
-    def self.rebuild_metrics(reject_older_than=7.days.ago)
-      @metrics['actions'] = []
-      @metrics['counters'] = []
+  # Rebuilds the metrics hash.
+  #
+  # You can specify a threshold number of days ago, such as `7.days.ago` as a
+  # parameter to this method. The default is `nil`, which indicates that ALL
+  # metrics from the log file should be returned, for as far back as the log 
+  # file goes.
+  #
+  def self.rebuild_metrics(reject_older_than=nil)
+    rebuild_start_time = Time.now
 
-      all_logged_metrics = File.read(HowSlow.full_path_to_log_file).lines.collect{|line| JSON.parse(line) unless line.start_with?("#")}
-      puts all_logged_metrics.class
-      puts all_logged_metrics.first.inspect
-      puts all_logged_metrics.inspect
-      all_logged_metrics.reject!{|metric| metric.nil? or Time.parse(metric['datetime']) < reject_older_than} unless reject_older_than.nil?
-      all_logged_metrics.each{|metric| @metrics[metric['type']].push(metric)}
-    end
+    @metrics['actions'] = []
+    @metrics['counters'] = []
+
+    all_logged_lines = File.read(HowSlow.full_path_to_log_file).lines
+    all_logged_metrics = []
+    all_logged_lines.each{|line| all_logged_metrics << JSON.parse(line) unless line.start_with?('#')}
+    puts all_logged_metrics.class
+    puts all_logged_metrics.first.inspect
+    puts all_logged_metrics.inspect
+    all_logged_metrics.reject!{|metric| Time.parse(metric['datetime']) < reject_older_than} unless reject_older_than.nil?
+    all_logged_metrics.each{|metric| @metrics[metric['type']] << metric}
+
+    @metrics['latest_rebuilt_runtime'] = Time.now-rebuild_start_time
+  end
 end
