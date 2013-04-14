7d6af1abc8e6e760103ccae5a55e8906ec217bdb
normalocity <jefflunt@gmail.com>
2013-04-14 00:08:44 -0500

    Reworked the filtering and rebuilding process
    - There are no longer any time filters on the rebuild_metrics method. All
      metrics will be loaded into memory.
    - As a result the time filter variable on the rebuild_metrics method has been
      removed.
    - The date and time filters have been retained on the slowest_actions methods,
      but slightly renamed to help with readability, and reversed to aid in the
      speed of the filtering unless a time filter is explicitly applied.
    - Added a comment about the @metrics hash being returned as an instance of
      HashWithIndifferentAccess.

--- lib/how_slow/reporter.rb
+++ lib/how_slow/reporter.rb
@@ -1,7 +1,7 @@
 module HowSlow
-  @metrics = {
-    'action' => [],
-    'counter' => []
+  @metrics = HashWithIndifferentAccess{
+    :action => [],
+    :counter => []
   }
 
   # Returns a hash of the metrics that have been logged to the log file. The 
@@ -9,18 +9,18 @@ module HowSlow
   # structure of the returned hash is:
   #
   #   {
-  #     'action' => [
-  #       {'type' => 'action', ... },
-  #       {'type' => 'action', ... },
+  #     :action => [
+  #       {'type' => :action, ... },
+  #       {'type' => :action, ... },
   #       ...
   #     ],
-  #     'counter' => [
-  #       {'type' => 'counter', ...},
+  #     :counter => [
+  #       {'type' => :counter, ...},
   #       ...
   #     ]
   #   }
   #
-  # The order of the metrics under each metric type key ('action', or 'counter')
+  # The order of the metrics under each metric type key (:action, or :counter)
   # is the same as the order in which they were written to the log file. NOTE
   # that this doesn't mean they're in perfect chronological order by the
   # datetime of the action since long-running actions, multiple threads,  or
@@ -33,7 +33,8 @@ module HowSlow
     @metrics
   end
  
-  # Gives you a list of the slowest actions by total_runtime, slowest first.
+  # Gives you a list of the slowest actions by :total_runtime, slowest first,
+  # filtered to metrics recorded between now and keep_since time ago.
   #
   # So, if you have the following set of action metrics in the log file...
   #
@@ -45,25 +46,30 @@ module HowSlow
   # 
   # ...then
   #
-  #   slowest_actions(nil, 2)
-  #   => [{'type':'action', 'total_runtime':456.7}, 
-  #       {'type':'action', 'total_runtime':123.0}]
+  #   slowest_actions(2)
+  #   => [{:type => 'action', :total_runtime => 456.7}, 
+  #       {:type => 'action', :total_runtime => 123.0}]
+  #
+  # The default number_of_actions to return is 5 and it determines the maximum
+  # number of metrics that will be returned by this method.
+  #
+  # The default keep_since value is nil, indicating that ALL logged metrics
+  # should be considered. Pass in a value such as 7.days.ago to limit the
+  # returned metrics to those whose :datetime attribute is within the last 7
+  # days.
   # 
-  def self.slowest_actions(reject_older_than=7.days.ago, number_of_slowest=5)
-    sorted_metrics = @metrics['action'].sort_by{|action| action['total_runtime']}
-    sorted_metrics.last(number_of_slowest).reverse
+  def self.slowest_actions(number_of_actions=5, keep_since=nil)
+    sorted_metrics = keep_since.nil? ? @metrics : @metrics.reject{|metric| metric[:datetime] < keep_since}
+    sorted_metrics = sorted_metrics[:action].sort_by{|action| action['total_runtime']}
+    sorted_metrics.last(number_of_actions).reverse
   end
 
-  # Rebuilds the metrics hash.
+  # Rebuilds the metrics hash, returning it as an instance of
+  # ActiveSupport::HashWithIndifferentAccess
   #
-  # You can specify a threshold number of days ago, such as `7.days.ago` as a
-  # parameter to this method. The default is `nil`, which indicates that ALL
-  # metrics from the log file should be returned, for as far back as the log 
-  # file goes.
-  #
-  def self.rebuild_metrics(reject_older_than=nil)
-    @metrics['action'] = []
-    @metrics['counter'] = []
+  def self.rebuild_metrics
+    @metrics[:action] = []
+    @metrics[:counter] = []
 
     all_logged_lines = File.read(HowSlow.full_path_to_log_file).lines
     all_logged_metrics = []
