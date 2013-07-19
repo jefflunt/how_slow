acc257ee337ffa3adafaf6928ec22722e4dddb4b
Jeff Lunt <jefflunt@gmail.com>
2013-07-19 09:31:34 -0500

    Ref #16 - Small tweaks

--- lib/how_slow/metrics/counter.rb
+++ lib/how_slow/metrics/counter.rb
@@ -1,7 +1,7 @@
 module HowSlow
   module Metrics
     # A metric for storing a count of something, such as every time a button is
-    # pressed so that you can count how many times a given features in your app
+    # pressed, so that you can count how many times a given features in your app
     # is being used.
     #
     # count - the count of this event
@@ -19,6 +19,7 @@ module HowSlow
       # Provides a string representation of this Counter metric, in the format of:
       # [event_name] :: [count]A
       # e.g. new login :: 73
+      #
       def to_default_email_string
         "#{am.event_name} :: #{am.count}"
       end
